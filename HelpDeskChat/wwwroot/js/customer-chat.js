bd23806439d9a7489bf35bdb796bb58151ff03f5
Felipe Mello <felipesilva.mello27@gmail.com>
2023-10-24 22:41:07 -0300

    Separação do código JS do chat Customer e Employee em dois arquivos diferentes

---
+++ HelpDeskChat/wwwroot/js/customer-chat.js
@@ -0,0 +1,97 @@
+﻿"use strict"
+
+var customerConnection;
+
+function startCustomerChat() {
+    customerConnection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").withAutomaticReconnect().build();
+    customerConnection.start()
+        .then(() => {
+            console.info("Connected");
+            var chatInfo = JSON.parse(sessionStorage.getItem("chatInfo"));
+            if (chatInfo) {
+                customerConnection.invoke("NewConnectionId", chatInfo.id);
+                console.info(chatInfo.id, typeof chatInfo.id);
+            }
+            enableCustomerChat();
+        })
+        .catch((err) => {
+            console.error(err.toString());
+            setTimeout(() => startCustomerChat(), 5000);
+        })
+}
+
+function enableCustomerChat() {
+    const chatContainer = document.getElementById('chat-container');
+    const chatHeader = document.getElementById('chat-header');
+    const closeButton = document.getElementById('close-button');
+    const chatBox = document.getElementById('chat-box');
+    const messageInput = document.getElementById('message-input');
+    const sendButton = document.getElementById('send-button');
+    const toggleButton = document.getElementById('toggle-button');
+
+    chatContainer.style.display = 'none';
+    toggleButton.style.display = 'block';
+
+    toggleButton.addEventListener('click', function () {
+        chatContainer.style.display = 'block';
+        toggleButton.style.display = 'none';
+    });
+
+    chatHeader.addEventListener('click', function () {
+        chatContainer.style.display = 'none';
+        toggleButton.style.display = 'block';
+    });
+
+    sendButton.addEventListener('click', function () {
+        const message = messageInput.value;
+        if (message) {
+            sendMessage(message);
+            messageInput.value = '';
+        }
+    });
+
+    customerConnection.on("Sent", (sent, message, chatId) => {
+        if (sent) {
+            var htmlMessage = '<div class="message user-message">Você: ' + message + '</div >';
+            $("#chat-box").append(htmlMessage);
+            $("#chat-box")[0].scrollTop = 9999999;
+
+            var chatInfo = { id: chatId };
+            sessionStorage.setItem("chatInfo", JSON.stringify(chatInfo));
+        }
+    })
+
+    customerConnection.on("CustomerReceive", (message, chatId) => {
+        var htmlMessage = '<div class="message other-message">Agente: ' + message + '</div >';
+        $("#chat-box").append(htmlMessage);
+        chatBox.scrollTop = 9999999;
+    })
+
+    customerConnection.on("ChatClosed", (closed) => {
+        console.info(closed);
+        if (closed) {
+            var messageInput = $(`#message-input`)[0];
+            var sendButton = $(`#send-button`)[0];
+
+            messageInput.disabled = true;
+            messageInput.value = "Chat encerrado."
+            sendButton.disabled = true;
+
+            sessionStorage.removeItem("chatInfo");
+        }
+    })
+}
+
+function sendMessage(message) {
+    var chatInfo = JSON.parse(sessionStorage.getItem("chatInfo"));
+    var chatId;
+
+    if (chatInfo) {
+        chatId = chatInfo.id;
+    }
+    else {
+        chatId = 0;
+    }
+
+    customerConnection.invoke("SendMessage", message, chatId);
+}
--- HelpDeskChat/wwwroot/js/employee-chat.js
+++ HelpDeskChat/wwwroot/js/employee-chat.js
@@ -1,10 +1,76 @@
 ﻿"use strict"
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").withAutomaticReconnect().build();
+var employeeConnection;
 var currentChatId;
 var isConnected = false;
 
-start();
+function startEmployeeChat() {
+    employeeConnection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").withAutomaticReconnect().build();
+
+    employeeConnection.start()
+        .then(() => {
+            console.info("Connected");
+            enableEmployeeChat();
+        })
+        .catch((err) => {
+            console.error(err.toString());
+            setTimeout(() => startEmployeeChat(), 5000);
+        })
+}
+
+function enableEmployeeChat() {
+    var messageInput = $(`#message-input${currentChatId}`)[0];
+    var sendButton = $(`#send-button${currentChatId}`)[0];
+
+    if (sendButton) {
+        sendButton.addEventListener('click', () => {
+            var messageText = messageInput.value;
+            if (messageText) {
+                sendMessageToCustomer(messageText);
+                messageInput.value = '';
+            }
+        })
+    }
+
+    console.info("Chat iniciado");
+    employeeConnection.on("EmployeesReceive", (message, chatId) => {
+        var html = `<li class="list-group-item" id="${chatId}" onclick="showChat('${chatId}')">Chat ${chatId} <strong style="color:green" id="newMessages${chatId}"></strong></li>`;
+
+        var chatItem = $(`#${chatId}`)[0];
+        if (!chatItem) {
+            $(".list-group").prepend(html);
+        } else {
+            html = $(`#${chatId}`)[0];
+            $(`#${chatId}`).remove();
+            $(".list-group").prepend(html);
+        }
+
+        var htmlMessage;
+        if (chatId == currentChatId) {
+            htmlMessage = `<div class="message other-message">Cliente: ${message}</div >`;
+
+            $(`#chat-container${chatId}`).append(htmlMessage);
+        } else {
+            var count = $(`#newMessages${chatId}`).text();
+
+            $(`#newMessages${chatId}`).html(++count);
+        }
+
+        $(`#chat-container${chatId}`).scrollTop = 9999999;
+    })
+
+    employeeConnection.on("EmployeesSent", (sent, message, chatId) => {
+        if (sent) {
+            var htmlMessage = `<div class="message user-message">Você: ${message}</div >`;
+            $(`#chat-container${chatId}`).append(htmlMessage);
+            $(`#chat-container${chatId}`)[0].scrollTop = 9999999;
+
+            var html = $(`#${chatId}`)[0];
+            $(`#${chatId}`).remove();
+            $(".list-group").prepend(html);
+        }
+    })
+}
 
 function changeChats() {
     var selectChats = document.querySelector("#selectChats");
@@ -42,154 +108,8 @@ function showClosedChats() {
     })
 }
 
-function start() {
-    connection.start()
-        .then(() => {
-            console.info("Connected");
-            var chatInfo = JSON.parse(sessionStorage.getItem("chatInfo"));
-            if (chatInfo) {
-                connection.invoke("NewConnectionId", chatInfo.id);
-                console.info(chatInfo.id, typeof chatInfo.id);
-            }
-        })
-        .catch((err) => {
-            console.error(err.toString());
-            setTimeout(() => start(), 5000);
-        })
-}
-
-function sendMessage(message) {
-    var chatInfo = JSON.parse(sessionStorage.getItem("chatInfo"));
-    var chatId;
-
-    if (chatInfo) {
-        chatId = chatInfo.id;
-    }
-    else {
-        chatId = 0;
-    }
-
-    connection.invoke("SendMessage", message, chatId);
-}
-
 function sendMessageToCustomer(message) {
-    connection.invoke("SendMessageToCustomer", message, parseInt(currentChatId));
-}
-
-function enableEmployeeChat() {
-    var messageInput = $(`#message-input${currentChatId}`)[0];
-    var sendButton = $(`#send-button${currentChatId}`)[0];
-
-    if (sendButton) {
-        sendButton.addEventListener('click', () => {
-            var messageText = messageInput.value;
-            if (messageText) {
-                sendMessageToCustomer(messageText);
-                messageInput.value = '';
-            }
-        })
-    }
-
-    console.info("Chat iniciado");
-    connection.on("EmployeesReceive", (message, chatId) => {
-        var html = `<li class="list-group-item" id="${chatId}" onclick="showChat('${chatId}')">Chat ${chatId} <strong style="color:green" id="newMessages${chatId}"></strong></li>`;
-
-        var chatItem = $(`#${chatId}`)[0];
-        if (!chatItem) {
-            $(".list-group").prepend(html);
-        } else {
-            html = $(`#${chatId}`)[0];
-            $(`#${chatId}`).remove();
-            $(".list-group").prepend(html);
-        }
-
-        var htmlMessage;
-        if (chatId == currentChatId) {
-            htmlMessage = `<div class="message other-message">Cliente: ${message}</div >`;
-
-            $(`#chat-container${chatId}`).append(htmlMessage);
-        } else {
-            var count = $(`#newMessages${chatId}`).text();
-
-            $(`#newMessages${chatId}`).html(++count);
-        }
-
-        $(`#chat-container${chatId}`).scrollTop = 9999999;
-    })
-
-    connection.on("EmployeesSent", (sent, message, chatId) => {
-        if (sent) {
-            var htmlMessage = `<div class="message user-message">Você: ${message}</div >`;
-            $(`#chat-container${chatId}`).append(htmlMessage);
-            $(`#chat-container${chatId}`)[0].scrollTop = 9999999;
-
-            var html = $(`#${chatId}`)[0];
-            $(`#${chatId}`).remove();
-            $(".list-group").prepend(html);
-        }
-    })
-}
-
-function enableCustomerChat() {
-    const chatContainer = document.getElementById('chat-container');
-    const chatHeader = document.getElementById('chat-header');
-    const closeButton = document.getElementById('close-button');
-    const chatBox = document.getElementById('chat-box');
-    const messageInput = document.getElementById('message-input');
-    const sendButton = document.getElementById('send-button');
-    const toggleButton = document.getElementById('toggle-button');
-
-    chatContainer.style.display = 'none';
-    toggleButton.style.display = 'block';
-
-    toggleButton.addEventListener('click', function () {
-        chatContainer.style.display = 'block';
-        toggleButton.style.display = 'none';
-    });
-
-    chatHeader.addEventListener('click', function () {
-        chatContainer.style.display = 'none';
-        toggleButton.style.display = 'block';
-    });
-
-    sendButton.addEventListener('click', function () {
-        const message = messageInput.value;
-        if (message) {
-            sendMessage(message);
-            messageInput.value = '';
-        }
-    });
-
-    connection.on("Sent", (sent, message, chatId) => {
-        if (sent) {
-            var htmlMessage = '<div class="message user-message">Você: ' + message + '</div >';
-            $("#chat-box").append(htmlMessage);
-            $("#chat-box")[0].scrollTop = 9999999;
-
-            var chatInfo = { id: chatId };
-            sessionStorage.setItem("chatInfo", JSON.stringify(chatInfo));
-        }
-    })
-
-    connection.on("CustomerReceive", (message, chatId) => {
-        var htmlMessage = '<div class="message other-message">Agente: ' + message + '</div >';
-        $("#chat-box").append(htmlMessage);
-        chatBox.scrollTop = 9999999;
-    })
-
-    connection.on("ChatClosed", (closed) => {
-        console.info(closed);
-        if (closed) {
-            var messageInput = $(`#message-input`)[0];
-            var sendButton = $(`#send-button`)[0];
-
-            messageInput.disabled = true;
-            messageInput.value = "Chat encerrado."
-            sendButton.disabled = true;
-
-            sessionStorage.removeItem("chatInfo");
-        }
-    })
+    employeeConnection.invoke("SendMessageToCustomer", message, parseInt(currentChatId));
 }
 
 function showChat(chatId) {
@@ -197,9 +117,9 @@ function showChat(chatId) {
 
     $(`#newMessages${chatId}`).html('');
 
-    connection.invoke("GetMessagesByChatId", parseInt(chatId));
+    employeeConnection.invoke("GetMessagesByChatId", parseInt(chatId));
 
-    connection.on("ReceiveMessages", (messages, chat) => {
+    employeeConnection.on("ReceiveMessages", (messages, chat) => {
         var disabled = chat.closed ? "disabled" : ""
         var chatMessage = chat.closed ? "Chat encerrado" : "";
 
@@ -235,7 +155,7 @@ function showChat(chatId) {
         }
     })
 
-    connection.on("ChatClosed", (closed) => {
+    employeeConnection.on("ChatClosed", (closed) => {
         if (closed) {
             var endButton = $(`#end-button${currentChatId}`)[0];
             var messageInput = $(`#message-input${currentChatId}`)[0];
@@ -262,9 +182,9 @@ function showChat(chatId) {
 }
 
 function endChat(chatId) {
-    connection.invoke("EndChat", parseInt(chatId));
+    employeeConnection.invoke("EndChat", parseInt(chatId));
 
-    connection.on("ChatClosed", (closed) => {
+    employeeConnection.on("ChatClosed", (closed) => {
         if (closed) {
             var endButton = $(`#end-button${currentChatId}`)[0];
             var messageInput = $(`#message-input${currentChatId}`)[0];
